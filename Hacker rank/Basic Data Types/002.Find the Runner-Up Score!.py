b998c9aa4d880a634b8071c9cefc73a732e2f762
LKomes <111873821+LKomes@users.noreply.github.com>
2022-08-29 01:25:40 -0300

    Update 002.Find the Runner-Up Score!.py

--- Hacker rank/Basic Data Types/002.Find the Runner-Up Score!.py
+++ Hacker rank/Basic Data Types/002.Find the Runner-Up Score!.py
@@ -1,3 +1,4 @@
+#https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem?isFullScreen=true
 if __name__ == '__main__':
     n = int(raw_input())
     arr = map(int, raw_input().split())
