e30757aaf6f9eb587e47fc3ba3e6eb24afc45092
LKomes <111873821+LKomes@users.noreply.github.com>
2022-08-28 22:55:40 -0300

    Create Find the Runner-Up Score!.py
    https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem?isFullScreen=true

---
+++ Hacker rank/Basic Data Types/Find the Runner-Up Score!.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(raw_input())
+    arr = map(int, raw_input().split())
+res = arr [0]
+sec = -100
+for a in range (n):
+    if arr[a]>res:
+        sec = res
+        res = arr[a]
+    elif arr[a]>sec and arr[a] != res:
+        sec = arr[a]
+print sec  
