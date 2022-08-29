4b46fcfe4656e110ae628746b3eb6599d6095b40
LKomes <111873821+LKomes@users.noreply.github.com>
2022-08-29 00:27:32 -0300

    Create 005.Find a string.py
    https://www.hackerrank.com/challenges/find-a-string/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen

---
+++ Hacker rank/Strings/005.Find a string.py
@@ -0,0 +1,17 @@
+def count_substring(string, sub_string):
+    a = 0
+    for i in range (len (string) - len(sub_string) + 1):
+        teste = 0
+        for n in range (len (sub_string)):
+            if string[i+n] == sub_string[n]:
+                teste += 1
+        if teste == len (sub_string):
+            a += 1        
+    return a
+
+if __name__ == '__main__':
+    string = raw_input().strip()
+    sub_string = raw_input().strip()
+    
+    count = count_substring(string, sub_string)
+    print count
