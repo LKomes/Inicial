e618bbae375ee88593d788a2bbb7268dd15c5a31
LKomes <111873821+LKomes@users.noreply.github.com>
2022-08-28 22:56:41 -0300

    Create 003.Nested Lists.py
    https://www.hackerrank.com/challenges/nested-list/problem?isFullScreen=true

---
+++ Hacker rank/Basic Data Types/003.Nested Lists.py
@@ -0,0 +1,22 @@
+if __name__ == '__main__':
+    list_scores = []
+    list_names = []
+    lst = []
+    dicti = {}
+    for _ in range(int(raw_input())):
+        name = raw_input()
+        score = float(raw_input())
+        dicti[name] = score
+        list_scores.append (score)
+        list_names.append (name)
+minimo = 100
+for a in list_scores:
+    if minimo>a:
+        minimo=a
+b=min(i for i in list_scores if i != min(list_scores))
+
+for g in list_names:
+    if dicti[g] == b:
+        lst.append(g)
+for s in sorted(lst):
+    print s   
